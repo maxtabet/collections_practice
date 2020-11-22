73fa4dd518a2fa258423444f37e721842850c9ad
Maximilian Tabet <maxtabet@gmail.com>
2020-11-21 22:50:42 -0500

    commit tree 2defc9cd389c45999d37ff89adc8eab2c7edd9b5

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+    newvar = array[1]
+    array[1] = array[2]
+    array[2] = newvar
+    return array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+        array.each {|word| word[2] = "$"} 
+    return array  
+end
+
+def find_a(array, start_char = 'a')
+    array.select {|word| word[0] == start_char}
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array, add_char = 's', except_index = 1)
+    array.each_index do |i|
+        unless i == except_index
+            array[i] << add_char
+        end
+    end
+end
+
+
+
 
